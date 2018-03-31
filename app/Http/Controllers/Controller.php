59ef7a233230199b1eb36f7ae2ae356532e0c72a
Mario Peñate Fariñas <ckwanted@gmail.com>
2018-03-31 01:45:15 +0100

    Install and config aws s3

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Storage;
 
 class Controller extends BaseController {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
@@ -17,12 +18,27 @@ class Controller extends BaseController {
      * @param User $user
      * @return bool
      */
-    public function is_me(User $user) {
+    protected function is_me(User $user) {
         $me = auth()->user();
 
         if( ($me->getRol() == "admin") || ($me->id == $user->id)) return true;
 
         return false;
+    }
+
+    protected function getAmazonUrlToken($uri) {
+        $s3 = Storage::disk('s3');
+        $client = $s3->getDriver()->getAdapter()->getClient();
+        $expiry = "+10 minutes";
+
+        $command = $client->getCommand('GetObject', [
+            'Bucket' => config('filesystems.disks.s3.bucket'),
+            'Key'    => $uri
+        ]);
+
+        $request = $client->createPresignedRequest($command, $expiry);
+
+        return (string) $request->getUri();
     }
 
 }
