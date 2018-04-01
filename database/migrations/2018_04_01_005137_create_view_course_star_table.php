da57789d488d4d2bdf00ec9cfaa7b33c4e2a07ab
Mario Peñate Fariñas <ckwanted@gmail.com>
2018-04-01 02:20:39 +0100

    Create course start sql view

---
+++ database/migrations/2018_04_01_005137_create_view_course_star_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateViewCourseStarTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        DB::statement("
+            CREATE VIEW view_course_star AS 
+            SELECT course_id, ROUND( AVG(value), 2) AS star, COUNT(course_id) AS votes
+            FROM course_stars
+            GROUP BY course_id
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        DB::statement("DROP VIEW IF EXISTS view_course_star");
+    }
+}
